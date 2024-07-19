4b6bb300d818d97cd592b9dbef8ed60d0787c41f
Jagrit Chaitanya <jchaitanya@hotmail.com>
2024-07-18 20:17:47 -0400

    Implemented a better scraper
    The scraper now writes to a json file. Ideally we want to directly write to the database.

--- backend/scraper/test.py
+++ backend/scraper/test.py
@@ -1,14 +1,45 @@
 import google.generativeai as genai
 import requests
+import os
+from dotenv import load_dotenv
+from bs4 import BeautifulSoup
 
-GOOGLE_API_KEY =  'nice_try_stealing_my_api_key'
+
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_GEMINI_API_KEY")
 genai.configure(api_key=GOOGLE_API_KEY)
 
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel(
+    "gemini-1.5-flash", generation_config={"response_mime_type": "application/json"}
+)
 
-site = 'https://xingfutang.com/menu'
+site = "https://chatime.ca/menu/"
 response = requests.get(site)
-raw_html = response.text
+raw_html_bytes = response.content
 
-response = model.generate_content("given the following raw html of a webpage that is a menu for a bubble tea store, output only a list of drinks and their associated descriptions:\n" + raw_html)
+soup = BeautifulSoup(raw_html_bytes, "html.parser")
+
+print("soup's type", type(soup.prettify()))
+
+prompt = (
+    """
+Given the following raw html of a webpage that is a menu for a bubble tea store, Using the this JSON schema:
+{
+    "drink_name": "string",
+    "drink_description": "string",
+    "image_link": "string"
+}
+Return the drinks, their associated descriptions, and a link to their image in JSON format. \n
+"""
+    + soup.prettify()
+)
+
+response = model.generate_content(prompt)
+
 print(response.text)
+# extract the txt into json file, with drink name, drink description and image link
+
+# write the json file
+with open("chatime_menu.json", "w") as f:
+    f.write(response.text)
